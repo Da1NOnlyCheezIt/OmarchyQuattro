c20105b447e40ee3356172ecae2cb85e3b20505b
Da1NOnlyCheezIt <theoneandonlycheezeit@gmail.com>
2026-08-30 19:06:21 -0600

    config updates

--- .config/hypr/windows.lua
+++ .config/hypr/windows.lua
@@ -1,6 +1,8 @@
 hl.window_rule({name  = "gamescope_ws1", match = { title = "^(gamescope|Deadlock|Terraria)$" },workspace = 1})
-hl.window_rule({match = {class = [[^([Ss]team)$]]},tile = true,workspace = 2})
-hl.window_rule({match = {class = [[^([Dd]iscord)$]]},tile = true,workspace = 3})
+hl.window_rule({match = {initial_title = [[([Ss]team)]]},tile = true,workspace = 3})
+hl.window_rule({match = {initial_title = [[([Dd]iscord)]]},tile = true,workspace = 2})
+hl.window_rule({match = {initial_title = [[.*([Cc]orsair).*]]},tile = true, workspace = 5})
+hl.window_rule({match = {initial_title = [[.*(PipeWire|OBS).*]]},tile = true, workspace = 4})
 hl.window_rule({
     match = { class= [[^(.*)$]]},
     immediate = true,
